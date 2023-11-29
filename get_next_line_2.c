a351406c03432d38536a230923ae0a0fd23ba03e
MarouanZelouani <marouanzelouani01@gmail.com>
2023-11-29 14:09:10 +0100

    finaly no leaks but......

--- get_next_line_2.c
+++ get_next_line_2.c
@@ -7,6 +7,7 @@
 #define BUFFER_SIZE 5
 #endif
 
+
 typedef struct s_list 
 {
     char * content;
@@ -20,31 +21,35 @@ void ft_add_to_lst(t_list **lst, char *holder, int read_cout);
 int found_newline(t_list *lst);
 t_list *ft_get_last_node(t_list *lst);
 void ft_allocat_line(t_list *lst, char **line);
-void ft_clear_list(t_list **lst);
+//void ft_clear_list(t_list *lst);
+size_t	ft_strlen(const char *s);
 
 /*...................................................*/
 
 char *get_next_line(int fd)
 {
-    static t_list *lst;
+    static t_list *lst; 
     char *line;
+    //char *holder;
 
-    lst = NULL;
     line = NULL;
     if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, line, 0) == -1)
         return (NULL);
-
-    // read 7ta tl9a \n
+    //int eof = read(fd, holder, BUFFER_SIZE);
+    //printf("%d\n", eof);
+    // if (eof == 0)
+    // {
+    //     line = ft_extract_line_fd(lst);
+    //     free(lst->content);
+    //     free(lst);
+    //     lst = NULL;
+    //     return (line);
+    // }
     ft_read_fd(&lst, fd);
     if (lst == NULL)
         return (NULL);
-
-    // 9ad line mn dakchi li 9raiti bi read
     line = ft_extract_line_fd(lst);
-
-    // clean lst dyalk o kheli dakchi li wra \n
     ft_clear_all_fd(&lst);
-
     return (line);
 }
 
@@ -52,23 +57,40 @@ void ft_read_fd(t_list **lst, int fd)
 {
     char *holder;
     int read_count;
+    char *line;
 
+    line = NULL;
     holder = malloc((BUFFER_SIZE + 1) * sizeof(char));
     if (holder == NULL)
         return ;
-    
     while (!found_newline(*lst))
     {
         read_count = read(fd, holder, BUFFER_SIZE);
+        //printf("n\n%d\n", read_count);
+        
         if (!read_count)
         {
+            //printf("noooo\n");
             free(holder);
+            char *line = ft_extract_line_fd(*lst);
+            //printf("%s\n", line);
+            free(line);
+            t_list *tmp;
+            while ((*lst) != NULL)
+            {
+                tmp = *lst;
+                *lst = (*lst)->next;
+                free(tmp->content);
+                free(tmp);
+            }
+            // lst = NULL;
             return ;
         }
         holder[read_count] = '\0';
         //add fi lekhar dyal lst
         ft_add_to_lst(lst, holder, read_count);
     }
+    //printf("%d\n", read_count);
     free(holder);
 }
 
@@ -79,6 +101,18 @@ char *ft_extract_line_fd(t_list *lst)
     int i;
     int j;
 
+    // if (lst == NULL)
+    // {
+    //     printf("=====================\n");
+    //     printf("null\n");
+    //     printf("=====================\n");
+    // }
+    // else 
+    // {
+    //     printf("=====================\n");
+    //     printf("%s\n", (lst)->content);
+    //     printf("=====================\n");
+    // }
     ft_allocat_line(lst, &line);
     if (line == NULL)
         return (NULL);
@@ -129,81 +163,148 @@ void ft_allocat_line(t_list *lst, char **line)
 }
 /*---------------------------------clean up-------------------------------*/
 
+// void ft_clear_all_fd(t_list **lst)
+// {
+//     t_list *node;
+//     t_list *new;
+//     char *holder;
+//     int i;
+//     int j;
+
+//     node = ft_get_last_node (*lst);
+//     new = malloc(sizeof(t_list));
+//     if (new == NULL)
+//         return ;
+//     new->content = malloc((BUFFER_SIZE + 1) * sizeof(char));
+//     new->next = NULL;
+//     if (new->content == NULL)
+//         return ;
+//     i = 0;
+//     while (node->content[i] && node->content[i] != '\n')
+//         i++;
+//     if (node->content && node->content[i] == '\n')
+//         i++;
+//     j = 0;
+//     while (node->content[i])
+//     {
+//         new->content[j] = node->content[i];
+//         i++;
+//         j++;
+//     }
+//     new->content[j] = '\0';
+//     // printf("----\n");
+//     // printf("%s", new->content);
+//     // printf("-----\n");
+
+//     // if (node)
+//     // {
+//     // }
+//     //free(holder);
+//     // printf("lst %p\n", lst);
+//     // printf("*lst %p\n", *lst);
+//     ft_clear_list(*lst);
+//     *lst = new; 
+
+//     // printf("=====================\n");
+//     // printf("%s\n", new->content);
+//     // printf("=====================\n");
+
+    
+//     //free(node->content);
+//     // free(node);
+//     // if (new->content[0])
+//     //     *lst = new;
+//     // else 
+//     // {
+//     //     free(new->content);
+//     //     free(new);
+//     // }
+
+
+//     // while (lst != NULL)
+// 	// {
+// 	// 	next = *lst->next;
+// 	// 	ft_lstdelone(node, del);
+//     //     free(*lst-);
+// 	// 	node = next;
+// 	// }
+// 	// *lst = NULL;
+// }
+
 void ft_clear_all_fd(t_list **lst)
 {
-    t_list *node;
-    t_list *new;
-    char *holder;
-    int i;
-    int j;
+    t_list  *current;
+    t_list  *tmp;
+    char    *holder;
+    char    *new_content;
+    int     i;
+    int     len;
 
-    node = ft_get_last_node (*lst);
-    new = malloc(sizeof(t_list));
-    holder = malloc((BUFFER_SIZE + 1) * sizeof(char));
-    if (new == NULL || holder == NULL)
-        return ;
+    tmp = NULL;
+    current = ft_get_last_node(*lst);
+
+    holder = current->content;
     i = 0;
-    while (node->content[i] && node->content[i] != '\n')
+    while (holder[i] && holder[i] != '\n')
         i++;
-    if (node->content[i] == '\n')
+    if (holder[i] && holder[i] == '\n')
         i++;
-    j = 0;
-    while (node->content[i])
+    
+    len = ft_strlen(holder + i);
+    new_content = malloc((len + 1) * sizeof(char));
+    if (new_content == NULL)
+        return ;
+
+    len = 0;
+    while (holder[i])
     {
-        holder[j] = node->content[i];
+        new_content[len] = holder[i];
+        len++;
         i++;
-        j++;
     }
-    holder[j] = '\0';
-    new->content = holder;
-    new->next = NULL;
-    // printf("----\n");
-    // printf("%s", new->content);
-    // printf("-----\n");
+    new_content[len] = '\0';
+    free(current->content);
+    current->content = new_content;
 
-    // if (node)
-    // {
-    // }
-    //free(holder);
-    // printf("lst %p\n", lst);
-    // printf("*lst %p\n", *lst);
-    ft_clear_list(lst);
+    // printf("=====================\n");
+    // printf("%s\n", current->content);
+    // printf("=====================\n");
 
-    //free(node->content);
-    // free(node);
-    if (new->content[0])
-        *lst = new;
-    else 
+    while ((*lst) != current)
     {
-        free(new->content);
-        free(new);
+        tmp = *lst;
+        *lst = (*lst)->next;
+        free(tmp->content);
+        free(tmp);
     }
-    //*lst = new; 
-
-    // while (lst != NULL)
-	// {
-	// 	next = *lst->next;
-	// 	ft_lstdelone(node, del);
-    //     free(*lst-);
-	// 	node = next;
-	// }
-	// *lst = NULL;
 }
 
-void ft_clear_list(t_list **lst)
+size_t	ft_strlen(const char *s)
 {
-	t_list	*a;
+	size_t	len;
 
-    a = NULL;
-	while ((*lst) != NULL)
-	{
-        a = (*lst)->next;
-        free((*lst)->content);
-        free(*lst);
-        (*lst) = a;
-	}
-    *lst = NULL;
+    if (!s)
+        return (0);
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
 }
+
+// void ft_clear_list(t_list *lst)
+// {
+//     t_list	*current;
+// 	t_list	*next;
+
+// 	current = lst;
+// 	while (current)
+// 	{
+// 		free(current->content);
+// 		next = current->next;
+// 		free(current);
+// 		current = next;
+// 	}
+// }
 
 /*------------------------------------------------------------------------*/
 
@@ -231,6 +332,8 @@ t_list *ft_get_last_node(t_list *lst)
     t_list	*node;
 
 	node = NULL;
+    if (lst == NULL)
+        return (NULL);
 	while (lst)
 	{
 		if (lst->next == NULL)
@@ -273,12 +376,12 @@ void ft_add_to_lst(t_list **lst, char *holder, int read_count)
 
 int main ()
 {
-    t_list *head;
+    //t_list *head;
     // t_list *node;
     // t_list *node2;
     // t_list *node1;    //  head -> node1 -> node -> NULL
 
-    head = NULL;
+    //head = NULL;
     
     // node2 = malloc(sizeof(t_list));
     // char ttt[] = "marouanzelouan";
@@ -301,14 +404,33 @@ int main ()
     // ft_read_fd(&head, fd);
     // char *line = ft_extract_line_fd(head);
     
+
     char *line = get_next_line(fd);
     printf("%s", line);
     free(line);
 
-    line = get_next_line(fd);
-    printf("%s", line);
-    free(line);
+    char *line1 = get_next_line(fd);
+    printf("%s", line1);
+    free(line1);
+
+    char *line2 = get_next_line(fd);
+    printf("%s", line2);
+    free(line2);
+
+    char *line3 = get_next_line(fd);
+    printf("%s", line3);
+    free(line3);
+
+    char *line4 = get_next_line(fd);
+    printf("%s", line4);
+    free(line4);
+
+
+    char *line5 = get_next_line(fd);
+    printf("%s", line5);
+    free(line5);
     
+
     printf("--------------------------------------------\n");
     //clearing the list
     // ft_clear_all_fd(&head);
---
+++ hh
@@ -0,0 +1,16 @@
+marou -> an ze -> louan -> i dr'' -> d\nnor -> null
+return = marouan zelouani dr d\n
+nor -> null
+
+
+nor -> ''inti -> ''mana -> ''bno\n -> null
+return = nor init mana bno\n
+null
+
+
+gg ni -> g\naym -> null
+return = gg nig\n 
+aym -> null
+
+
+
